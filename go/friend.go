a0edd56fd663532b1b36936356827186237e5ea9
naoto67 <uver.com67@gmail.com>
2020-09-01 13:21:36 +0900

    friendMapの型変更

--- go/friend.go
+++ go/friend.go
@@ -1,24 +1,26 @@
 package main
 
-func FetchFriendDict(userID int) (map[int]bool, error) {
-	res := map[int]bool{}
+import "time"
+
+func FetchFriendDict(userID int) (map[int]time.Time, error) {
+	res := map[int]time.Time{}
 	relations := []Relation{}
-	err := db.Select(&relations, "SELECT another FROM relations WHERE one = ?", userID)
+	err := db.Select(&relations, "SELECT another, created_at FROM relations WHERE one = ?", userID)
 	if err != nil {
 		return nil, err
 	}
 	for _, v := range relations {
-		res[v.Another] = true
+		res[v.Another] = v.CreatedAt
 	}
 	return res, nil
 }
 
-func isFriendInDict(friendDict map[int]bool, targetID int) bool {
+func isFriendInDict(friendDict map[int]time.Time, targetID int) bool {
 	_, ok := friendDict[targetID]
 	return ok
 }
 
-func isPermitted(friendDict map[int]bool, selfID, targetID int) bool {
+func isPermitted(friendDict map[int]time.Time, selfID, targetID int) bool {
 	if selfID == targetID {
 		return true
 	}
