78f66977aaed674b9ba20fb2fc2d9e9b0d6d40b4
naoto67 <uver.com67@gmail.com>
2020-09-01 15:56:15 +0900

    修正

--- go/comment.go
+++ go/comment.go
@@ -4,9 +4,18 @@ import "github.com/jmoiron/sqlx"
 
 func FetchCommentsForMe(userID int) (comments []Comment, err error) {
 	var entryIDs []int
-	err = db.Select(entryIDs, "SELECT id FROM entries WHERE user_id = ?", userID)
+	rows, err := db.Query("SELECT id FROM entries WHERE user_id = ?", userID)
 	if err != nil {
-		return
+		return nil, err
+	}
+	for rows.Next() {
+		var entryID int
+		err = rows.Scan(&entryID)
+		if err != nil {
+			return
+		}
+
+		entryIDs = append(entryIDs, entryID)
 	}
 	logger.Infow("FetchCommentsForMe", "entryIDs", entryIDs)
 	query := "SELECT * FROM comments WHERE entry_id IN (?) ORDER BY created_at DESC LIMIT 10"
