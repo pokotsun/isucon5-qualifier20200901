ad4e5e9ad825d8c32f1fc61e7e0821acb6a0e254
pokotsun <gorigolila@gmail.com>
2020-09-05 16:41:29 +0900

    remove unused columns

--- go/app.go
+++ go/app.go
@@ -260,7 +260,7 @@ func GetIndex(w http.ResponseWriter, r *http.Request) {
 	}
 
 	entries := []Entry{}
-	err = db.Select(&entries, `SELECT * FROM entries WHERE user_id = ? ORDER BY created_at LIMIT 5`, user.ID)
+	err = db.Select(&entries, `SELECT id, userID, private, title, created_at  FROM entries WHERE user_id = ? ORDER BY created_at LIMIT 5`, user.ID)
 	if err != sql.ErrNoRows {
 		checkErr(err)
 	}
@@ -282,7 +282,7 @@ LIMIT 10`, user.ID)
 	}
 	rows.Close()
 
-	err = db.Select(&entries, `SELECT * FROM entries ORDER BY created_at DESC LIMIT 1000`)
+	err = db.Select(&entries, `SELECT id, userID, private, title, created_at FROM entries ORDER BY created_at DESC LIMIT 1000`)
 	if err != sql.ErrNoRows {
 		checkErr(err)
 	}
@@ -310,7 +310,7 @@ LIMIT 10`, user.ID)
 			continue
 		}
 		var entry Entry
-		err := db.Get(&entry, `SELECT * FROM entries WHERE id = ?`, c.EntryID)
+		err := db.Get(&entry, `SELECT private FROM entries WHERE id = ?`, c.EntryID)
 		checkErr(err)
 		if entry.Private {
 			if !permitted(w, r, entry.UserID) {
