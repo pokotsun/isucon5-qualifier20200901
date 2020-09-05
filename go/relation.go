eb9c5737bc2a969e4fbb53cde7d360e69bb2b05f
naoto67 <uver.com67@gmail.com>
2020-09-05 15:02:50 +0900

    ポインタ渡すように修正

--- go/relation.go
+++ go/relation.go
@@ -14,7 +14,7 @@ func FetchFriendMap(userID int) (map[int]time.Time, error) {
 		var anotherID int
 		var createdAt time.Time
 
-		if err := rows.Scan(anotherID, createdAt); err != nil {
+		if err := rows.Scan(&anotherID, &createdAt); err != nil {
 			return nil, err
 		}
 		res[anotherID] = createdAt
